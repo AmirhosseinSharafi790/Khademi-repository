e08ba352aa63aec406f7df64f1cdd38397036a7f
amirhossein <theamir790@gmail.com>
2024-07-04 22:22:44 +0430

    add files to repo

---
+++ src/components/header/Header.js
@@ -0,0 +1,189 @@
+import "./Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../assets/images/Logo.jpg";
+import Aos from "aos";
+import { useEffect } from "react";
+import { FaPhoneFlip, FaRegNewspaper } from "react-icons/fa6";
+import { SlPicture } from "react-icons/sl";
+import { MdConnectWithoutContact, MdPermContactCalendar } from "react-icons/md";
+import { TfiCup } from "react-icons/tfi";
+import { IoHomeOutline } from "react-icons/io5";
+// search-Box-Import
+// import Form from "react-bootstrap/Form";
+// import { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+// import Col from "react-bootstrap/esm/Col";
+// import Row from "react-bootstrap/esm/Row";
+function Header() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+  // search-Box-State
+  // const [show, setShow] = useState(false);
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  const expand = "lg";
+  return (
+    <>
+      <Navbar expand={expand} className="py-3 bg-nav mb-5">
+        <Container className="m-0 m-auto">
+          {/* دیوی که نوبار تاگل و نوبار برند ، توش هست ، بعد از اینکه سرچ باکس گذاشته شد ، باید حذف شود */}
+          <div className="d-flex justify-content-start  align-items-center">
+            <Navbar.Toggle
+              className="border-0 fs-4"
+              aria-controls={`offcanvasNavbar-expand-${expand}`}
+            />
+            <Navbar.Brand href="#">
+              <Link to="/">
+                <img
+                  src={Logo}
+                  className="img-fluid border-logo shadow"
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    borderColor: "#346ed1",
+                    borderRadius: "50%",
+                  }}
+                  data-aos="fade-left"
+                  data-aos-duration="1000"
+                  alt=""
+                />
+              </Link>
+            </Navbar.Brand>
+          </div>
+          <NavLink
+            to="tel:+03536217013"
+            className="btn-login mx-1"
+            id="display"
+            data-aos="fade-right"
+          >
+            <span className="header-phone">035-36217013</span>
+            <span className="mx-1">
+              <FaPhoneFlip size={"20px"} className="custom-icon-style" />
+            </span>
+          </NavLink>
+          {/* search-Box-Modal */}
+          {/* <div className="my-2" id="display">
+                        <Button onClick={handleShow} className="ms-4" style={{backgroundColor : "transparent" , border : "none"}}>
+                            <i className="bi bi-search text-dark fs-4 fw-bold bg-nav"></i>
+                        </Button>
+                        <Modal show={show} onHide={handleClose} className="bg-opacity-10 bg-danger">
+                            <Modal.Header closeButton>
+                            </Modal.Header>
+                            <Modal.Body>
+                            <Form inline>
+                                <Row className="justify-content-center align-items-center">
+                                    <Col xs="auto">
+                                        <Form.Control
+                                        style={{width: "275px" , outline : "0" , boxShadow : "none"}}
+                                        type="text"
+                                        placeholder="دنبال چی میگردی ؟ اینجا بنویس"
+                                        className="mr-sm-2 bg-color p-3 border"
+                                        />
+                                    </Col>
+                                    <Col xs="auto">
+                                        <Button type="submit"  style={{backgroundColor : "transparent" , border : "none"}}>
+                                            <i className="bi bi-search fs-4 text-dark"></i>
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                            </Modal.Body>
+                        </Modal>
+                </div> */}
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                هنرستان فنی نمونه دولتی خادمی
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="d-flex flex-grow-1 justify-content-center custom-navbar-item-style">
+                <NavLink to={"/"} className="nav-link">
+                  <IoHomeOutline
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  صفحه نخست
+                </NavLink>
+                <NavLink to={"/honors"} className="nav-link" id="dsply1">
+                  <TfiCup
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  افتخارات
+                </NavLink>
+                <NavLink to={"/album"} className="nav-link">
+                  <SlPicture
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  گالری تصاویر
+                </NavLink>
+                <NavLink to={"/news"} className="nav-link">
+                  <FaRegNewspaper
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  اخبار و اطلاعیه
+                </NavLink>
+                {/* <NavLink to={"/registration"} className="nav-link">
+                  پیش ثبت نام
+                </NavLink> */}
+                <NavLink to={"/aboutus"} className="nav-link" id="dsply2">
+                  <MdPermContactCalendar
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  درباره هنرستان
+                </NavLink>
+                <NavLink to={"/contact"} className="nav-link">
+                  <MdConnectWithoutContact
+                    className="me-2 custom-media-queris"
+                    size={"20px"}
+                    color="#346ed175"
+                  />
+                  ارتباط با هنرستان
+                </NavLink>
+                {/* <NavLink to={"/grading"} className="nav-link" id="dsply4">نمره دهی</NavLink>
+                      <NavLink to={"/rollcall"} className="nav-link" id="dsply5">سامانه حضور و غیاب</NavLink> */}
+              </Nav>
+              <div className="my-1" id="menu-hide">
+                <NavLink
+                  to="tel:+03536217013"
+                  className="btn-login mx-1"
+                  data-aos="fade-right"
+                >
+                  <span className="mx-1">035-36217013</span>
+                  <span className="mx-1">
+                    <FaPhoneFlip size={"20px"} className="custom-icon-style" />
+                  </span>
+                </NavLink>
+                {/* search-Box-Icon */}
+                {/* <Button onClick={handleShow} className="ms-4" style={{backgroundColor : "transparent" , border : "none"}}>
+                        <i className="bi bi-search text-dark fs-4 bg-nav"></i>
+                    </Button> */}
+              </div>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+export default Header;
---
+++ src/pages/Map/Map.js
@@ -0,0 +1,13 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+  const position = [32.027274, 54.196393];
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </MapContainer>
+  );
+};
+
+export default Map;
